5d2cb755d4559b928925bcb88b37cc0c6119d0ad
kn777 <kn_777@docomo.ne.jp>
2018-07-02 17:58:39 +0900

    2018/7/2

--- DBConnector/src/Test.java
+++ DBConnector/src/Test.java
@@ -3,10 +3,12 @@ public class Test {
 	public static void main(String[] args){
 		TestUserDAO dao = new TestUserDAO();
 		dao.select("taro", "123");
-		dao.selectALL();
+		dao.selectAll();
 		dao.selectByName("taro");
 		dao.selectByPassword("123");
-		dao.updateNameByUserName("taro","saburo");
+		dao.updateUserNameByUserName("taro","saburo");
+		dao.insert(4, "shiro", "012");
+		dao.dalete("shiro");
 		}
 }
 
